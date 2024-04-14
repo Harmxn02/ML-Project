e8ebeabe9fcbf280cb70444e49c0ef0e6d026361
Harman Singh <harman.singh@student.howest.be>
2024-04-14 18:22:03 +0200

    Refactor code: Remove commented-out code

--- util/calculate_scores.py
+++ util/calculate_scores.py
@@ -9,12 +9,9 @@ def calculate_scores(df):
 
         df[f"net_score{i}"] = df[hotel_pos_cols].values - df[hotel_neg_cols].values
     
-    # combined_net_score = df[pos_columns].sum(axis=1) - df[neg_columns].sum(axis=1)
-
     # Adding new columns to the DataFrame
     df["combined_pos_score"] = df[pos_columns].sum(axis=1)
     df["combined_neg_score"] = df[neg_columns].sum(axis=1)
-    # df["combined_net_score"] = combined_net_score
 
     # Dropping original score columns and net score columns
     df.drop(columns=pos_columns + neg_columns + [f'net_score{i}' for i in range(1, 6)], inplace=True)
